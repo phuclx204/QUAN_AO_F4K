3ae1df8750e06c422e3d7c7f49e44e74a9484f8f
trumdameto <lxp18001@gmail.com>
2024-09-27 10:10:29 +0700

    Add project quan_ao_f4k

---
+++ src/main/java/org/example/quan_ao_f4k/QuanAoF4kApplication.java
@@ -0,0 +1,13 @@
+package org.example.quan_ao_f4k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanAoF4kApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanAoF4kApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/quan_ao_f4k/QuanAoF4kApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.quan_ao_f4k;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanAoF4kApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
