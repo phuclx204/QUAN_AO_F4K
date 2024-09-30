364892b8dccda6cf327cfd3857b69e2963f570d1
sonngph2212 <sonngph31538@gmail.com>
2024-09-30 20:43:34 +0700

    demo ui admin site

---
+++ src/main/java/org/example/quan_ao_f4k/config/ShopF4KWebMvcConfigurer.java
@@ -0,0 +1,7 @@
+package org.example.quan_ao_f4k.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class ShopF4KWebMvcConfigurer implements WebMvcConfigurer {
+
+}
