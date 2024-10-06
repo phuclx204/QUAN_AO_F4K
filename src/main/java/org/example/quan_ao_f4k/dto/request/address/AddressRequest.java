356b5eabce80fe508452e941b410090ac51ee800
trumdameto <lxp18001@gmail.com>
2024-10-06 18:07:25 +0700

    merge

--- src/main/java/org/example/quan_ao_f4k/dto/request/address/AddressRequest.java
+++ src/main/java/org/example/quan_ao_f4k/dto/request/address/AddressRequest.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class AddressRequest {
 	private String line;
-	private String provinceId;
-	private String districtId;
-	private String wardId;
+	private Long provinceId;
+	private Long districtId;
+	private Long wardId;
 }
