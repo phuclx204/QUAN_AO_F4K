8030ecc9fe9a06b98fba0b5293d96f2cc82b28cd
trumdameto <lxp18001@gmail.com>
2024-11-23 02:11:38 +0700

    kh nv

--- src/main/java/org/example/quan_ao_f4k/dto/request/employee/EmployeeRequest.java
+++
@@ -1,8 +0,0 @@
-package org.example.quan_ao_f4k.dto.request.employee;
-
-import lombok.Data;
-
-@Data
-public class EmployeeRequest {
-	private Long userId;
-}
--- src/main/java/org/example/quan_ao_f4k/dto/response/employee/EmployeeResponse.java
+++
@@ -1,4 +0,0 @@
-package org.example.quan_ao_f4k.dto.response.employee;
-
-public class EmployeeResponse {
-}
